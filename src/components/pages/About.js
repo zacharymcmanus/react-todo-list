0e75a918eb64df4fc27db15e83e4aca6d34a736e
Zachary McManus <zacharymcmanus@Zachs-MacBook.local>
2019-01-10 22:12:17 -0800

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>This is the React Todo List app v1.0.0</h1>
+    </React.Fragment>
+  )
+}
+
+export default About;
